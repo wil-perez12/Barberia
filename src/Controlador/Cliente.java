052939223465e4450080fe0057cd508e1f8ae2ba
ing Wilnel <perezwilnel12@gmail.com>
2024-11-25 08:59:34 -0400

    cambios

--- src/Controlador/Cliente.java
+++ src/Controlador/Cliente.java
@@ -2,21 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Controlador;
-
-/**
- *
- * @author Admin
- */
-public class Cliente extends Usuario {
-
-    public Cliente(String nombre, String apellido, String correo, String telefono, String contraseña) {
-        super(nombre, apellido, correo, telefono, contraseña, "Cliente");
-    }
-
-    // Método para agendar cita
-    public void agendarCita() {
-        System.out.println("Cita agendada por el cliente " + nombre);
-    }
-}
+//package Controlador;
+//
+///**
+// *
+// * @author Admin
+// */
+//public class Cliente extends Usuario {
+//
+//    public Cliente(String nombre, String apellido, String correo, String telefono, String contraseña) {
+//        super(nombre, apellido, correo, telefono, contraseña, "Cliente");
+//    }
+//
+//    // Método para agendar cita
+//    public void agendarCita() {
+//        System.out.println("Cita agendada por el cliente " + nombre);
+//    }
+//}
 
